0cd183e29c39517b93bfb111d0ec74190a27e4b9
sasi-kanakinti <9908135565ks@gmail.com>
2025-04-28 14:51:59 +0530

    Updated Getters and setters in Mobile prepaid

--- mobileprepaid/src/main/java/com/aits/mobileprepaid/entity/RechargePlan.java
+++ mobileprepaid/src/main/java/com/aits/mobileprepaid/entity/RechargePlan.java
@@ -23,6 +23,57 @@ public class RechargePlan {
 	private double price;
 	
 	private int validityInDays;
+
+	public RechargePlan(Integer id, String name, String category, double price, int validityInDays) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.category = category;
+		this.price = price;
+		this.validityInDays = validityInDays;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getValidityInDays() {
+		return validityInDays;
+	}
+
+	public void setValidityInDays(int validityInDays) {
+		this.validityInDays = validityInDays;
+	}
+	
+	public RechargePlan() {}
 	
 	
 	
--- mobileprepaid/src/main/java/com/aits/mobileprepaid/entity/User.java
+++ mobileprepaid/src/main/java/com/aits/mobileprepaid/entity/User.java
@@ -34,6 +34,76 @@ public class User {
     
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<RechargeHistory> history;
+
+	public User(Long id, String name, String mobile, String email, String password, Role role,
+			List<RechargeHistory> history) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mobile = mobile;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.history = history;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public List<RechargeHistory> getHistory() {
+		return history;
+	}
+
+	public void setHistory(List<RechargeHistory> history) {
+		this.history = history;
+	}
+
+	public User() {}
    
    
 
